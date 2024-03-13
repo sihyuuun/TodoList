bc3ab376cc077fcdcc049a18dc121654bbe53ef9
Sihyun Lee <leesh3597@gmail.com>
2024-03-13 14:00:43 +0900

    Initial Commit

---
+++ Todo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Todo
+//
+//  Created by 이시현 on 3/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by 이시현 on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
